88f4c3aaf93ff580a166f8f59ec9fc1891b68a36
Robert Schindler <r.schindler@efficiosoft.com>
2020-09-29 12:30:51 +0200

    Warn when building uWSGI from a pre-cloned repository

--- scripts/build_uwsgi.sh
+++ scripts/build_uwsgi.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -e
 # Build a custom uWSGI with all plugins required.
+# The patch for CGI chunked request handling, needed for git-http-backend, wasn't
+# released yet, hence uWSGI needs to be built from master.
 
 PLUGINS="cgi corerouter python router_cache router_rewrite router_uwsgi ugreen"
 
-# Contains patch for CGI chunked request handling, needed for git-http-backend
-[ -d uwsgi ] || git clone --depth 1 --single-branch --no-tags https://github.com/unbit/uwsgi
+if [ -d uwsgi ]; then
+	echo "WARNING: uwsgi directory exists, building without cloning" >&2
+else
+	git clone --depth 1 --no-tags https://github.com/unbit/uwsgi
+fi
+
 cd uwsgi
 python3 uwsgiconfig.py --build core
 for plugin in $PLUGINS; do
