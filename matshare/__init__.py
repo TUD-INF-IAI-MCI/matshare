579095b09da1932e1d6babff19f25e065864b6a5
Robert Schindler <r.schindler@efficiosoft.com>
2020-10-08 17:03:55 +0200

    Release v0.1.1

--- matshare/__init__.py
+++ matshare/__init__.py
@@ -2,7 +2,7 @@
 Root package of the MatShare project.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # AppConfig to use when including "matshare" in INSTALLED_APPS
 default_app_config = "matshare.apps.MatShareConfig"
